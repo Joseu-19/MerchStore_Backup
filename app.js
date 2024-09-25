c88a95f6b7bd5d718468fc087f9fd27fa8b427de
Joseu-19 <95296240+Joseu-19@users.noreply.github.com>
2024-09-25 18:03:57 -0400

    Added more styles to the pages, just need to add media queries.

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('pages/about');
 });
-// ######################################################
+// ####################shop route##################################
 app.get('/shop', (req, res) => {
     getDbConnection()
     .then((db) => {
